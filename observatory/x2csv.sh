f2b25eb4c87d548bf5da815a1474d7c47ef2992d
hiroro9 <hiroro9@gmail.com>
2020-07-26 11:01:09 +0900

    edit x2csv.sh

--- observatory/x2csv.sh
+++ observatory/x2csv.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 #xlsx2csv -p "" -I "Data" -a FILENAME.xlsx | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > CSVNAME.csv
-xlsx2csv -p "" -I "Data" -a T01*.xlsx | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > T01.csv
+echo "Please input Sensor ID!"
+read SID
+
+INPUT_FILE=$SID*.xlsx
+echo $INPUT_FILE
+
+case "$SID" in
+  T0*)
+  xlsx2csv -p "" -I "Data" -a $INPUT_FILE | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > "$SID-full-obs.csv"
+  ;;
+  TD0*)
+  xlsx2csv -p "" -I "Data" -a $INPUT_FILE| sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature,Pressure,Sea pressure,Depth//g" -e '/^$/d' > "$SID-full-obs.csv"
+  ;;
+  *) echo "Please input valid ID!"
+  ;;
+esac
+
