32f53f33a16b194a75b0e8ea55099f3499bd0ddd
hiroro9 <hiroro9@gmail.com>
2020-08-06 15:17:25 +0900

    modify x2csv.sh

--- observatory/x2csv.sh
+++ observatory/x2csv.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Please input Sensor ID!"
 read SID
+echo "Please input Obs No.!"
+read OBS
 
 INPUT_FILE="$SID*.xlsx"
 
@@ -8,10 +10,10 @@ if [ -f ${INPUT_FILE} ] ; then
   echo $INPUT_FILE
   case "$SID" in
     T0*)
-    xlsx2csv -p "" -I "Data" -a $INPUT_FILE | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > "$SID-full-obs.csv"
+    xlsx2csv -p "" -I "Data" -a $INPUT_FILE | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > "$SID_$OBS_obs_full.csv"
     ;;
     TD0*)
-    xlsx2csv -p "" -I "Data" -a $INPUT_FILE| sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature,Pressure,Sea pressure,Depth//g" -e '/^$/d' > "$SID-full-obs.csv"
+    xlsx2csv -p "" -I "Data" -a $INPUT_FILE| sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature,Pressure,Sea pressure,Depth//g" -e '/^$/d' > "$SID_$OBS_obs_full.csv"
     ;;
     *) echo "Please input valid ID!"
     ;;
