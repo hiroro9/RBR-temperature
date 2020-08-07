83043f71be14d14af91489e35867b30caa4be18e
hiroro9 <hiroro9@gmail.com>
2020-08-07 11:45:21 +0900

    modify batch files

--- observatory/x2csv.sh
+++ observatory/x2csv.sh
@@ -9,14 +9,19 @@ INPUT_FILE="$SID*.xlsx"
 if [ -f ${INPUT_FILE} ] ; then
   echo $INPUT_FILE
   case "$SID" in
-    T0*)
-    xlsx2csv -p "" -I "Data" -a $INPUT_FILE | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > "${SID}_${OBS}_obs_full.csv"
-    ;;
-    TD0*)
+
+    TD*)
     xlsx2csv -p "" -I "Data" -a $INPUT_FILE| sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature,Pressure,Sea pressure,Depth//g" -e '/^$/d' > "${SID}_${OBS}_obs_full.csv"
     ;;
+
+    T*)
+    xlsx2csv -p "" -I "Data" -a $INPUT_FILE | sed -e "4,$ s/Data//g" -e "4,$ s/Time,Temperature//g" -e '/^$/d' > "${SID}_${OBS}_obs_full.csv"
+    ;;
+
+    
     *) echo "Please input valid ID!"
     ;;
+
   esac
 
 else
